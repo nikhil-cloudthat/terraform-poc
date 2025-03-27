4d7e2c26019023d1b6021b8c469c16185c4bfd7f
Nikhil-CT <nikhil.n@cloudthat.com>
2025-03-27 17:54:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "ap-south-1"
+  region = "ap-south-1"
 }
 
 data "aws_security_group" "sg" {
@@ -7,9 +7,8 @@ data "aws_security_group" "sg" {
 }
 
 data "aws_ami" "al2023-ami-2023" {
-  owners           = var.amifilter["owner"]
-  #name_regex       = "^ami-[0-9a-z]{16}[0-9a-z]$"
-  most_recent      = true
+  owners      = var.amifilter["owner"]
+  most_recent = true
 
   filter {
     name   = "name"
@@ -30,37 +29,25 @@ data "aws_ami" "al2023-ami-2023" {
     name   = "root-device-type"
     values = var.amifilter["rootdevice"]
   }
-
 }
 
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.al2023-ami-2023.id
-  instance_type = var.instancetype
-  subnet_id     = "subnet-00064131485745db1"
+  ami                         = data.aws_ami.al2023-ami-2023.id
+  instance_type               = var.instancetype
+  subnet_id                   = "subnet-00064131485745db1"
   associate_public_ip_address = var.ipassociate
-  monitoring    = var.monitor
-  key_name = var.keypair
+  monitoring                  = var.monitor
+  key_name                    = var.keypair
 
-  security_groups = [
-    data.aws_security_group.sg.id
-  ]
-  
+  security_groups = [data.aws_security_group.sg.id]
+
   root_block_device {
-    #device_name = "/dev/xvda"          #(for ebs_block_device)
     delete_on_termination = var.volumedetails["delete_with_instance"]
-    volume_type = var.volumedetails["type"]
-    volume_size = var.volumedetails["size"]
-
-    # tags = {
-    #         Name = "instance-01a /dev/xvda"
-    #     }
-
+    volume_type           = var.volumedetails["type"]
+    volume_size           = var.volumedetails["size"]
   }
 
   tags = {
     Name = var.instanceName
   }
 }
-
-
